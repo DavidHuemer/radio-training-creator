caf562add016cfdf08073bf533083ed5ca46fc24
David Huemer <s2110307068@students.fh-hagenberg.at>
2022-10-21 10:50:28 +0200

    Added auth and home routing to the app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
+import {canActivate, redirectLoggedInTo, redirectUnauthorizedTo} from "@angular/fire/auth-guard";
 
-const routes: Routes = [];
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth/sign-in']);
+const redirectLoggedInToHome = () => redirectLoggedInTo(['home']);
+
+const routes: Routes = [
+  {path: '', redirectTo: 'auth/sign-in', pathMatch: 'full'},
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    ...canActivate(redirectLoggedInToHome)
+  },
+  {
+    path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule),
+    ...canActivate(redirectUnauthorizedToLogin)
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
