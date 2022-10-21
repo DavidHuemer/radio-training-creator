bb86194df624836c25028e3cfd521ca9af1d6f41
David Huemer <s2110307068@students.fh-hagenberg.at>
2022-10-21 10:36:38 +0200

    Added firebase to project

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
+  firebase: {
+    projectId: 'radio-training-creator',
+    appId: '1:745697238441:web:3a64252f9b1e78aa7bf8b8',
+    storageBucket: 'radio-training-creator.appspot.com',
+    locationId: 'europe-west',
+    apiKey: 'AIzaSyCCMJhwRpAfXfsKhB7lE1VdhufokfmIK9E',
+    authDomain: 'radio-training-creator.firebaseapp.com',
+    messagingSenderId: '745697238441',
+  },
   production: true
 };
