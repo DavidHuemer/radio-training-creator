534330890844b61264ebbc09f035563dbf7fe72e
David Huemer <92david.huemer@gmail.com>
2022-10-26 22:37:16 +0200

    Added a route to the projects page

--- src/app/home/home-routing.module.ts
+++ src/app/home/home-routing.module.ts
@@ -1,9 +1,15 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {HomePageComponent} from "./pages/home-page/home-page.component";
+import {ProjectsPageComponent} from "./pages/projects-page/projects-page.component";
 
 const routes: Routes = [
-  {path: '', component: HomePageComponent},
+  {
+    path: '', component: HomePageComponent, children: [
+      {path: '', redirectTo: 'projects', pathMatch: 'full'},
+      {path: 'projects', component: ProjectsPageComponent}
+    ]
+  },
 ];
 
 @NgModule({
