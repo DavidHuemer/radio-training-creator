5cb821afc38dd0842b978f8c25d738db1e81dc6b
David Huemer <92david.huemer@gmail.com>
2022-10-26 22:34:02 +0200

    Added basic projects page

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,11 +4,13 @@ import {CommonModule} from '@angular/common';
 import {HomeRoutingModule} from './home-routing.module';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import {MatIconModule} from "@angular/material/icon";
+import { ProjectsPageComponent } from './pages/projects-page/projects-page.component';
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    ProjectsPageComponent
   ],
   imports: [
     CommonModule,
