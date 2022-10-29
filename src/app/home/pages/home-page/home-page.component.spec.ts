994f28a34aba22d43240bf5639f536d3ff8259b4
David Huemer <92david.huemer@gmail.com>
2022-10-29 15:00:48 +0200

    The home page now updates the user in the global state

--- src/app/home/pages/home-page/home-page.component.spec.ts
+++ src/app/home/pages/home-page/home-page.component.spec.ts
@@ -1,14 +1,34 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {HomePageComponent} from './home-page.component';
+import {GLOBAL_RX_STATE, GlobalState} from "../../../core/states/GlobalState";
+import {AuthService} from "../../../core/services/auth/auth.service";
+import {UserService} from "../../../core/services/auth/user.service";
+import {RxState} from "@rx-angular/state";
+import SpyObj = jasmine.SpyObj;
+import {of} from "rxjs";
+import {User} from "../../../core/data/User";
 
 describe('HomePageComponent', () => {
   let component: HomePageComponent;
   let fixture: ComponentFixture<HomePageComponent>;
 
+  let globalStateMock: RxState<GlobalState>;
+  let authServiceMock: SpyObj<AuthService>;
+  let userServiceMock: SpyObj<UserService>;
+
   beforeEach(async () => {
+    globalStateMock = new RxState<GlobalState>();
+    authServiceMock = jasmine.createSpyObj('AuthService', ['getUserId']);
+    userServiceMock = jasmine.createSpyObj('UserService', ['getUserByUserId']);
+
     await TestBed.configureTestingModule({
-      declarations: [HomePageComponent]
+      declarations: [HomePageComponent],
+      providers: [
+        {provide: GLOBAL_RX_STATE, useValue: globalStateMock},
+        {provide: AuthService, useValue: authServiceMock},
+        {provide: UserService, useValue: userServiceMock}
+      ]
     })
       .compileComponents();
 
@@ -19,5 +39,30 @@ describe('HomePageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should set the user of the global state', async () => {
+    authServiceMock.getUserId.and.returnValue('exampleUserId');
+    const returningUser: User = {
+      documentId: 'exampleUserId',
+      lastName: 'mustermann',
+      firstName: 'max'
+    };
+
+    userServiceMock.getUserByUserId.and.returnValue(of(returningUser));
+    component.updateUser();
+    fixture.detectChanges();
+    globalStateMock.select('user').subscribe(x => {
+      expect(x).toBe(returningUser);
+    })
+  });
+
+  it('should set the user null when the current user is null', function () {
+    authServiceMock.getUserId.and.returnValue(undefined);
+    component.updateUser();
+    fixture.detectChanges();
+    globalStateMock.select('user').subscribe(x => {
+      expect(x).toBe(null);
+    })
   });
 });
--- src/app/home/pages/home-page/home-page.component.ts
+++ src/app/home/pages/home-page/home-page.component.ts
@@ -1,4 +1,8 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {GLOBAL_RX_STATE, GlobalState} from "../../../core/states/GlobalState";
+import {RxState} from "@rx-angular/state";
+import {AuthService} from "../../../core/services/auth/auth.service";
+import {UserService} from "../../../core/services/auth/user.service";
 
 @Component({
   selector: 'app-home-page',
@@ -7,10 +11,20 @@ import {Component, OnInit} from '@angular/core';
 })
 export class HomePageComponent implements OnInit {
 
-  constructor() {
+  constructor(@Inject(GLOBAL_RX_STATE) private globalState: RxState<GlobalState>, private authService: AuthService, private userService: UserService) {
   }
 
   ngOnInit(): void {
+    this.updateUser();
+  }
+
+  updateUser() {
+    const userId = this.authService.getUserId();
+    if (userId) {
+      this.globalState.connect('user', this.userService.getUserByUserId(userId));
+    } else {
+      this.globalState.set('user', () => null);
+    }
   }
 
 }
