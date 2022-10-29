cda14beb7e87cbea13a8537b201f4061b1f6af32
David Huemer <92david.huemer@gmail.com>
2022-10-29 15:00:22 +0200

    The projects page now extends the BaseAuthPage

--- src/app/home/pages/projects-page/projects-page.component.ts
+++ src/app/home/pages/projects-page/projects-page.component.ts
@@ -1,13 +1,17 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Inject, OnInit} from '@angular/core';
+import {BaseAuthPage} from "../../../shared/basics/BaseAuthPage";
+import {GLOBAL_RX_STATE, GlobalState} from "../../../core/states/GlobalState";
+import {RxState} from "@rx-angular/state";
 
 @Component({
   selector: 'app-projects-page',
   templateUrl: './projects-page.component.html',
   styleUrls: ['./projects-page.component.scss']
 })
-export class ProjectsPageComponent implements OnInit {
+export class ProjectsPageComponent extends BaseAuthPage implements OnInit {
 
-  constructor() {
+  constructor(@Inject(GLOBAL_RX_STATE) globalState: RxState<GlobalState>) {
+    super(globalState);
   }
 
   ngOnInit(): void {
