4cb6af060258d56eab27ac4c97de3c69b133a21b
David Huemer <92david.huemer@gmail.com>
2022-10-29 14:35:19 +0200

    The projects-page component now shows the correct username and title

--- src/app/home/pages/projects-page/projects-page.component.spec.ts
+++ src/app/home/pages/projects-page/projects-page.component.spec.ts
@@ -1,14 +1,30 @@
 import {ComponentFixture, TestBed} from '@angular/core/testing';
 
 import {ProjectsPageComponent} from './projects-page.component';
+import {GLOBAL_RX_STATE} from "../../../core/states/GlobalState";
+import {of} from "rxjs";
+import {User} from "../../../core/data/User";
 
 describe('ProjectsPageComponent', () => {
   let component: ProjectsPageComponent;
   let fixture: ComponentFixture<ProjectsPageComponent>;
 
+  let globalStateMock;
+
   beforeEach(async () => {
+    globalStateMock = jasmine.createSpyObj(['select']);
+    const user: User = {
+      documentId: '',
+      firstName: 'Max',
+      lastName: 'Mustermann'
+    };
+    globalStateMock.select.withArgs('user').and.returnValue(of(user))
+
     await TestBed.configureTestingModule({
-      declarations: [ProjectsPageComponent]
+      declarations: [ProjectsPageComponent],
+      providers: [
+        {provide: GLOBAL_RX_STATE, useValue: globalStateMock}
+      ]
     })
       .compileComponents();
 
@@ -19,5 +35,10 @@ describe('ProjectsPageComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show the username', async () => {
+    fixture.detectChanges();
+    expect(fixture.debugElement.childNodes["0"].nativeNode.userName).toBe('Max Mustermann');
   });
 });
