5356cc85d485eadab2dbece7654a3cd76085e282
David Huemer <92david.huemer@gmail.com>
2022-10-29 14:34:04 +0200

    Updated BaseFireStoreService

--- src/app/core/services/basics/BaseFirestoreService.ts
+++ src/app/core/services/basics/BaseFirestoreService.ts
@@ -1,6 +1,7 @@
-import {collection, doc, Firestore, setDoc} from "@angular/fire/firestore";
+import {collection, doc, docData, Firestore, setDoc} from "@angular/fire/firestore";
 import {CollectionReference} from "@angular/fire/firestore";
 import {DocumentReference} from "@angular/fire/firestore";
+import {map} from "rxjs";
 
 /**
  * Base class for all firestore services
@@ -9,6 +10,9 @@ export abstract class BaseFirestoreService {
   collectionRef = collection;
   docRef = doc;
   setDocRef = setDoc;
+  docDataRef = docData;
+
+  public static readonly DOCUMENT_ID_PROPERTY_NAME = 'documentId';
 
   protected constructor(private fireStore: Firestore) {
   }
@@ -42,5 +46,37 @@ export abstract class BaseFirestoreService {
    */
   protected getDocumentOfCollectionById(collectionRef: CollectionReference, id: string) {
     return this.docRef(collectionRef, id);
+  }
+
+  /**
+   * Returns a document data as a type by a given path
+   * @param path The path to the document
+   * @param pathSegments Optional path segments
+   * @protected
+   */
+  protected getDocumentDataAndParse<T>(path: string, ...pathSegments: string[]) {
+    return this.getDocumentData(path).pipe(
+      map((projectData) => ({...projectData as T}))
+    )
+  }
+
+  /**
+   * Returns a document data by a given path
+   * @param path The path to the document
+   * @param pathSegments Optional path segments
+   * @protected
+   */
+  protected getDocumentData(path: string, ...pathSegments: string[]) {
+    return this.docDataRef(this.getDocument(path, ...pathSegments), {idField: BaseFirestoreService.DOCUMENT_ID_PROPERTY_NAME});
+  }
+
+  /**
+   * Returns a document reference by a given path
+   * @param path The path to the document
+   * @param pathSegments Optional path segments
+   * @protected
+   */
+  protected getDocument(path: string, ...pathSegments: string[]) {
+    return this.docRef(this.fireStore, path, ...pathSegments);
   }
 }
